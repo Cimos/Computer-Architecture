8d08f8c447f2a7d777b6508e2c25806a1f004370
Simon Maddison <s3493550@student.rmit.edu.au>
2017-10-09 03:04:26 +1100

    *ALMOST DONE

--- CPU/lpm_decode1.vhd
+++ CPU/lpm_decode1.vhd
@@ -47,7 +47,9 @@ ENTITY lpm_decode1 IS
 		eq1		: OUT STD_LOGIC ;
 		eq2		: OUT STD_LOGIC ;
 		eq3		: OUT STD_LOGIC ;
-		eq4		: OUT STD_LOGIC 
+		eq4		: OUT STD_LOGIC ;
+		eq5		: OUT STD_LOGIC ;
+		eq6		: OUT STD_LOGIC 
 	);
 END lpm_decode1;
 
@@ -60,6 +62,8 @@ ARCHITECTURE SYN OF lpm_decode1 IS
 	SIGNAL sub_wire3	: STD_LOGIC ;
 	SIGNAL sub_wire4	: STD_LOGIC ;
 	SIGNAL sub_wire5	: STD_LOGIC ;
+	SIGNAL sub_wire6	: STD_LOGIC ;
+	SIGNAL sub_wire7	: STD_LOGIC ;
 
 
 
@@ -76,6 +80,8 @@ ARCHITECTURE SYN OF lpm_decode1 IS
 	END COMPONENT;
 
 BEGIN
+	sub_wire7    <= sub_wire0(6);
+	sub_wire6    <= sub_wire0(5);
 	sub_wire5    <= sub_wire0(4);
 	sub_wire4    <= sub_wire0(3);
 	sub_wire3    <= sub_wire0(2);
@@ -86,6 +92,8 @@ BEGIN
 	eq2    <= sub_wire3;
 	eq3    <= sub_wire4;
 	eq4    <= sub_wire5;
+	eq5    <= sub_wire6;
+	eq6    <= sub_wire7;
 
 	lpm_decode_component : lpm_decode
 	GENERIC MAP (
@@ -118,8 +126,8 @@ END SYN;
 -- Retrieval info: PRIVATE: eq2 NUMERIC "1"
 -- Retrieval info: PRIVATE: eq3 NUMERIC "1"
 -- Retrieval info: PRIVATE: eq4 NUMERIC "1"
--- Retrieval info: PRIVATE: eq5 NUMERIC "0"
--- Retrieval info: PRIVATE: eq6 NUMERIC "0"
+-- Retrieval info: PRIVATE: eq5 NUMERIC "1"
+-- Retrieval info: PRIVATE: eq6 NUMERIC "1"
 -- Retrieval info: PRIVATE: eq7 NUMERIC "0"
 -- Retrieval info: PRIVATE: nBit NUMERIC "3"
 -- Retrieval info: CONSTANT: LPM_DECODES NUMERIC "8"
@@ -132,12 +140,16 @@ END SYN;
 -- Retrieval info: USED_PORT: eq2 0 0 0 0 OUTPUT NODEFVAL eq2
 -- Retrieval info: USED_PORT: eq3 0 0 0 0 OUTPUT NODEFVAL eq3
 -- Retrieval info: USED_PORT: eq4 0 0 0 0 OUTPUT NODEFVAL eq4
+-- Retrieval info: USED_PORT: eq5 0 0 0 0 OUTPUT NODEFVAL eq5
+-- Retrieval info: USED_PORT: eq6 0 0 0 0 OUTPUT NODEFVAL eq6
 -- Retrieval info: CONNECT: @data 0 0 3 0 data 0 0 3 0
 -- Retrieval info: CONNECT: eq0 0 0 0 0 @eq 0 0 1 0
 -- Retrieval info: CONNECT: eq1 0 0 0 0 @eq 0 0 1 1
 -- Retrieval info: CONNECT: eq2 0 0 0 0 @eq 0 0 1 2
 -- Retrieval info: CONNECT: eq3 0 0 0 0 @eq 0 0 1 3
 -- Retrieval info: CONNECT: eq4 0 0 0 0 @eq 0 0 1 4
+-- Retrieval info: CONNECT: eq5 0 0 0 0 @eq 0 0 1 5
+-- Retrieval info: CONNECT: eq6 0 0 0 0 @eq 0 0 1 6
 -- Retrieval info: LIBRARY: lpm lpm.lpm_components.all
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_decode1.vhd TRUE
 -- Retrieval info: GEN_FILE: TYPE_NORMAL lpm_decode1.inc FALSE
